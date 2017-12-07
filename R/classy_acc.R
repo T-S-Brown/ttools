945a0f74c5ca415e0cd8ea4b985b40325764b1b4
Thomas Brown <tom.southwell.brown@gmail.com>
2017-12-07 19:44:09 +0000

    Add accuracy prediction and minor documentation updates

---
+++ R/classy_acc.R
@@ -0,0 +1,28 @@
+#' Classy Accuracy
+#'
+#' Quickly compute the accuracy of an algothim by indputing predicted and actual values of an outcome
+#' @param pred Vector of predicted outcomes
+#' @param real Outcomes the algortihm is attempting to predict
+#' @keywords classification metric
+#' @export
+#' @examples
+#'
+#'
+#' classy_acc(pred, real)
+#'
+
+classy_acc <- function(pred, real) {
+
+  result_data <- data.frame(pred,
+                            real)
+
+  result_data <- dplyr::mutate(results_data,
+                               outcome == case_when(
+                                 pred == real ~ 1,
+                                 TRUE ~ 0
+                               ))
+
+  accuracy = sum(result_data$outcome) / length(result_data$outcome)
+
+  return(accuracy)
+}
